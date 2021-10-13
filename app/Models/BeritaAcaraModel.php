4536da9cbfd1ca2e538dcd4d90d755c40ebed447
korospace <bagaselkoro@gmail.com>
2021-10-13 18:48:56 +0700

    edit berita acara model

--- app/Models/BeritaAcaraModel.php
+++ app/Models/BeritaAcaraModel.php
@@ -76,7 +76,7 @@ class BeritaAcaraModel extends Model
                 $berita = $this->db->table($this->table)->select("berita_acara.id,berita_acara.title,admin.nama_lengkap AS author,berita_acara.created_at,berita_acara.content,berita_acara.thumbnail")->join('admin', 'berita_acara.created_by = admin.id')->where("berita_acara.id",$get['id_berita'])->get()->getFirstRow();
             } 
             else if (isset($get['kategori']) && !isset($get['id_berita'])) {
-                $berita = $this->db->table($this->table)->select('id,title,created_at,thumbnail')->where("id_kategori",$get['kategori'])->orderBy('created_at','DESC')->get()->getResultArray();
+                $berita = $this->db->table($this->table)->select('berita_acara.id,berita_acara.title,berita_acara.created_at,berita_acara.thumbnail')->join('kategori_berita','berita_acara.id_kategori = kategori_berita.id')->where("kategori_berita.name",$get['kategori'])->orderBy('created_at','DESC')->get()->getResultArray();
             } 
             else {
                 $berita = $this->db->table($this->table)->select('berita_acara.id,berita_acara.title,kategori_berita.name AS kategori,berita_acara.created_at,berita_acara.thumbnail')->join('kategori_berita','berita_acara.id_kategori = kategori_berita.id')->orderBy('created_at','DESC')->get()->getResultArray();

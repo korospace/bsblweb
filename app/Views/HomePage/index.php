71a568850e7c86f3a3a1ad6021560934e4f7d329
riloanggoro <riloanggorosaputra@gmail.com>
2021-10-20 13:34:57 +0700

    Add OTP Form

--- app/Views/HomePage/index.php
+++ app/Views/HomePage/index.php
@@ -148,7 +148,7 @@
                 <img src="assets/images/features-icon-1.png" alt="">
                 <h4>WEBINAR</h4>
                 <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
-                <a href="" class="main-button">
+                <a href="<?= base_url('/');?>" class="main-button">
                   Read More
                 </a>
               </div>

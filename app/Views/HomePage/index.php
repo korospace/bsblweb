0867d91a07b300f1da7ddbb2b06ded861aadcb91
riloanggoro <riloanggorosaputra@gmail.com>
2021-10-21 20:31:45 +0700

    Fixing Footer

--- app/Views/HomePage/index.php
+++ app/Views/HomePage/index.php
@@ -38,7 +38,7 @@
             <nav class="main-nav">
               <!-- ***** Logo Start ***** -->
 
-              <a class="logo">
+              <a class="logo" href="<?= base_url('/');?>">
                 <img class="logo_nav" src="assets/images/banksampah-logo.png" alt="" width="65" height="55">
               </a>
               <!-- ***** Logo End ***** -->
@@ -339,46 +339,86 @@
 
     <!-- footer section starts  -->
 
-    <div class="footer">
-      <div class="box-container">
-        <div class="box">
-          <h3>about us</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet pariatur rerum consectetur architecto
-            ad
-            tempora blanditiis quo aliquid inventore a.</p>
-        </div>
-        <div class="box">
-          <h3>quick links</h3>
-          <a href="#welcome">Home</a>
-          <a href="#activity">Kegiatan</a>
-          <a href="#services">Layanan</a>
-          <a href="#contact-us">Contact Us</a>
-        </div>
-        <div class="box">
-          <h3>follow us</h3>
-          <a href="#">facebook</a>
-          <a href="#">instagram</a>
-          <a href="#">pinterest</a>
-          <a href="#">twitter</a>
-        </div>
-        <div class="box">
-          <h3>contact info</h3>
-          <div class="info">
-            <i class="fas fa-phone"></i>
-            <p> +62-456-7890 <br> +62-2222-333 </p>
-          </div>
-          <div class="info">
-            <i class="fas fa-envelope"></i>
-            <p> example@gmail.com <br> example@gmail.com </p>
-          </div>
-          <div class="info">
-            <i class="fas fa-map-marker-alt"></i>
-            <p> Jakarta, Indonesia - 12260 </p>
+    <footer class="deneb_footer">
+      <div class="widget_wrapper" style="background-image: url(assets/images/footer-bg.png);">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-6 col-12">
+              <div class="widget widegt_about">
+                <div class="widget_title">
+                  <img src="assets/images/logo_1.png" class="img-fluid" alt="">
+                  <p>Quisque orci nisl, viverra et sem ac, tincidunt egestas massa. Morbi est arcu, hendrerit ac vehicula condimentum, euismod nec tortor praesent consequat urna.</p>
+                </div>  
+                <ul class="social">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <div class="widget widget_link">
+                <div class="widget_title">
+                  <h4>Links</h4>
+                </div>
+                <ul>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="#activity">Kegiatan</a></li>
+                  <li><a href="#services">Layanan</a></li>
+                  <li><a href="#contact-us">Kontak Kami</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <div class="widget widget_contact">
+                <div class="widget_title">
+                  <h4>Contact Us</h4>
+                </div>
+                <div class="contact_info">
+                  <div class="single_info">
+                    <div class="icon">
+                      <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div class="info">
+                      <p><a>1800-121-3637</a></p>
+                      <p><a>+91 924-614-7999</a></p>
+                    </div>
+                  </div>
+                  <div class="single_info">
+                    <div class="icon">
+                      <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="info">
+                      <p><a href="mailto:info@deneb.com">bankasampahbudiluhur@gmail.com</a></p>
+                      <p><a href="mailto:services@deneb.com">bsblservice.com</a></p>
+                    </div>
+                  </div>
+                  <div class="single_info">
+                    <div class="icon">
+                      <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <div class="info">
+                      <p>125, Park street aven, Brocklyn,<span>Newyork.</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <h1 class="credit"> &copy; copyright @BankSampahBudiLuhur </h1>
-    </div>
+      <div class="copyright_area">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="copyright_text">
+                <p>Copyright &copy; 2020 All rights reserved.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 
     <!-- footer section ends -->
   </body>

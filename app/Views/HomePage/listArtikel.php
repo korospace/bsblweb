406503d387e949e872337c1e68ad1cacb4340c08
riloanggoro <riloanggorosaputra@gmail.com>
2021-11-11 09:33:14 +0700

    clean code

--- app/Views/HomePage/listArtikel.php
+++ app/Views/HomePage/listArtikel.php
@@ -2,100 +2,93 @@
 
 <!-- Css -->
 <?= $this->section('contentCss'); ?>
-	<style>
-		body {}
+<style>
+	body {}
 
-		section.wrapper {
-			margin-left: 0 !important;
-			margin-right: 0 !important;
-			min-height : 490px;
-		}
+	section.wrapper {
+		margin-left: 0 !important;
+		margin-right: 0 !important;
+		min-height: 490px;
+	}
 
-		@media (max-width:990px) {
-			body {
-				overflow: hidden;
-			}
+	@media (max-width:990px) {
+		body {
+			overflow: hidden;
 		}
-	</style>
-	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/css/flex-slider.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/css/owl-carousel.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/css/artikel.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/css/sidebar/style.css'); ?>">
+	}
+</style>
+<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/flex-slider.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/owl-carousel.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/artikel.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/sidebar/style.css'); ?>">
 <?= $this->endSection(); ?>
 
 <!-- JS -->
 <?= $this->section('contentJs'); ?>
-	<script>
-		const KATEGORI = '<?= $kategori; ?>';
-	</script>
-	<script src="<?= base_url('assets/js/plugins/font-awesome.min.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/plugins/waypoints.min.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/plugins/owl-carousel.min.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/plugins/imgfix.min.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/homepage.min.js'); ?>"></script>
+<script>
+	const KATEGORI = '<?= $kategori; ?>';
+</script>
+<script src="<?= base_url('assets/js/plugins/font-awesome.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/plugins/waypoints.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/plugins/owl-carousel.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/plugins/imgfix.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/homepage.min.js'); ?>"></script>
 
-	<script src="<?= base_url('assets/js/artikel.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/dropdown/bootstrap.min.js'); ?>"></script>
-	<script src="<?= base_url('assets/js/dropdown/popper.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/artikel.js'); ?>"></script>
+<script src="<?= base_url('assets/js/dropdown/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/dropdown/popper.min.js'); ?>"></script>
 <?= $this->endSection(); ?>
 
 <!-- Body -->
 <?= $this->section('content'); ?>
 
+<!-- **** Loading Spinner **** -->
+<?= $this->include('Components/loadingSpinner'); ?>
+<!-- **** Alert Info **** -->
+<?= $this->include('Components/alertInfo'); ?>
+
+<body class="">
+
 	<!-- **** Loading Spinner **** -->
 	<?= $this->include('Components/loadingSpinner'); ?>
-	<!-- **** Alert Info **** -->
+	<!-- **** Alert info **** -->
 	<?= $this->include('Components/alertInfo'); ?>
-	<body class="">
 
-		<!-- **** Loading Spinner **** -->
-		<?= $this->include('Components/loadingSpinner'); ?>
-		<!-- **** Alert info **** -->
-		<?= $this->include('Components/alertInfo'); ?>
-
-		<!-- ***** Header Area Start ***** -->
-		<header class="header-area header-sticky">
-			<div class="container col-lg-4 col-12">
-				<div class="row">
-					<div class="col-12">
-						<nav class="main-nav">
-							<!-- ***** Logo Start ***** -->
-							<a class="logo" href="<?= base_url('/'); ?> ">
-								<img class="logo_nav" src="<?= base_url('assets/images/banksampah-logo.webp'); ?>" alt=""
-									width="65" height="55">
-							</a>
-							<a class='menu-trigger'>
-								<span>Menu</span>
-							</a>
-							<!-- ***** Menu End ***** -->
-						</nav>
-					</div>
+	<!-- ***** Header Area Start ***** -->
+	<header class="header-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<nav class="main-nav">
+						<a class="logo" href="<?= base_url('/'); ?> ">
+							<img class="logo_nav" src="<?= base_url('assets/images/banksampah-logo.webp'); ?>" alt=""
+								width="65" height="55">
+						</a>
+						<a class='menu-trigger'>
+							<span>Menu</span>
+						</a>
+					</nav>
 				</div>
 			</div>
-		</header>
-		<!-- ***** Header Area End ***** -->
-
-		<!-- CARDS -->
-		<section class="wrapper d-flex justify-content-center align-items-center">
-			<div class="container pd-6">
-				<div class="row" id="container-article">
-					<div class="col-12 d-none" id="img-404">
-						<img src="<?= base_url('assets/images/404.jpg') ?>" alt="" style="max-width:100%; opacity:0.7;">
-					</div>
+		</div>
+	</header>
+	<section class="wrapper d-flex justify-content-center align-items-center">
+		<div class="container pd-6">
+			<div class="row" id="container-article">
+				<div class="col-9 d-none" id="img-404">
+					<img src="<?= base_url('assets/images/404.jpg') ?>" alt="" style="max-width:100%; opacity:0.7;">
 				</div>
 			</div>
-			<div class=" col-3">
-				<div class="main-sidebar">
-					<!-- Search -->
+		</div>
+		<div class=" col-3 align-self-start">
+				<div class="main-sidebar ">
 					<div class="single-widget search">
 						<div class="form">
 							<input type="email" placeholder="Search Here...">
 							<a class="button" href="#"><i class="fa fa-search"></i></a>
 						</div>
 					</div>
-					<!--/ End Search-->
-					<!-- Single Widget -->
 					<div class="single-widget category">
 						<h3 class="title">Artikel Lainnya</h3>
 						<ul class="categor-list">
@@ -104,116 +97,73 @@
 							<li><a href="#">Sosialisasi & Edukasi</a></li>
 						</ul>
 					</div>
-					<!--/ End Single Widget -->
-					<!-- Single Widget -->
-					<!-- <div class="single-widget recent-post">
-						<h3 class="title">Artikel Rekomendasi</h3>
-						<div class="single-post">
-							<div class="image">
-								<img src="https://via.placeholder.com/100x100" alt="#">
-							</div>
-							<div class="content">
-								<h5><a href="#">Lorem ipsum dolor sit amet.</a></h5>
-								<ul class="comment">
-									<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 11, 2020</li>
-								</ul>
-							</div>
-						</div>
-						<div class="single-post">
-							<div class="image">
-								<img src="https://via.placeholder.com/100x100" alt="#">
-							</div>
-							<div class="content">
-								<h5><a href="#">Lorem ipsum dolor sit amet.</a></h5>
-								<ul class="comment">
-									<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 11, 2020</li>
-								</ul>
-							</div>
-						</div>
-						<div class="single-post">
-							<div class="image">
-								<img src="https://via.placeholder.com/100x100" alt="#">
-							</div>
-							<div class="content">
-								<h5><a href="#">Lorem ipsum dolor sit amet.</a></h5>
-								<ul class="comment">
-									<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 11, 2020</li>
-								</ul>
-							</div>
-						</div>
-					</div> -->
 				</div>
 			</div>
-		</section>
-
-
-		<!-- footer section starts  -->
-
-		<footer class="deneb_footer">
-			<div class="widget_wrapper" style="background-image:url('<?= base_url('assets/images/footer-bg.png'); ?>');">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-4 col-md-6 col-12">
-							<div class="widget widegt_about">
-								<div class="widget_title">
-									<img src="assets/images/logo_1.png" class="img-fluid" alt="">
-									<p>Quisque orci nisl, viverra et sem ac, tincidunt egestas massa. Morbi est arcu,
-										hendrerit ac vehicula
-										condimentum, euismod nec tortor praesent consequat urna.</p>
-								</div>
-								<ul class="social">
-									<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-									<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fab fa-instagram"></i></a></li>
-								</ul>
+	</section>
+	<footer class="deneb_footer">
+		<div class="widget_wrapper" style="background-image:url('<?= base_url('assets/images/footer-bg.png'); ?>');">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-md-6 col-12">
+						<div class="widget widegt_about">
+							<div class="widget_title">
+								<img src="assets/images/logo_1.png" class="img-fluid" alt="">
+								<p>Quisque orci nisl, viverra et sem ac, tincidunt egestas massa. Morbi est arcu,
+									hendrerit ac vehicula
+									condimentum, euismod nec tortor praesent consequat urna.</p>
 							</div>
+							<ul class="social">
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							</ul>
 						</div>
-						<div class="col-lg-4 col-md-6 col-sm-12">
-							<div class="widget widget_link">
-								<div class="widget_title">
-									<h4>Links</h4>
-								</div>
-								<ul>
-									<li><a href="/">Home</a></li>
-									<li><a href="#activity">Kegiatan</a></li>
-									<li><a href="#services">Layanan</a></li>
-									<li><a href="#contact-us">Kontak Kami</a></li>
-								</ul>
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-12">
+						<div class="widget widget_link">
+							<div class="widget_title">
+								<h4>Links</h4>
 							</div>
+							<ul>
+								<li><a href="/">Home</a></li>
+								<li><a href="#activity">Kegiatan</a></li>
+								<li><a href="#services">Layanan</a></li>
+								<li><a href="#contact-us">Kontak Kami</a></li>
+							</ul>
 						</div>
-						<div class="col-lg-4 col-md-6 col-sm-12">
-							<div class="widget widget_contact">
-								<div class="widget_title">
-									<h4>Contact Us</h4>
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-12">
+						<div class="widget widget_contact">
+							<div class="widget_title">
+								<h4>Contact Us</h4>
+							</div>
+							<div class="contact_info">
+								<div class="single_info">
+									<div class="icon">
+										<i class="fas fa-phone-alt"></i>
+									</div>
+									<div class="info">
+										<p><a>1800-121-3637</a></p>
+										<p><a>+91 924-614-7999</a></p>
+									</div>
 								</div>
-								<div class="contact_info">
-									<div class="single_info">
-										<div class="icon">
-											<i class="fas fa-phone-alt"></i>
-										</div>
-										<div class="info">
-											<p><a>1800-121-3637</a></p>
-											<p><a>+91 924-614-7999</a></p>
-										</div>
+								<div class="single_info">
+									<div class="icon">
+										<i class="fas fa-envelope"></i>
 									</div>
-									<div class="single_info">
-										<div class="icon">
-											<i class="fas fa-envelope"></i>
-										</div>
-										<div class="info">
-											<p><a href="mailto:info@deneb.com">bankasampahbudiluhur@gmail.com</a></p>
-											<p><a href="mailto:services@deneb.com">bsblservice.com</a></p>
-										</div>
+									<div class="info">
+										<p><a href="mailto:info@deneb.com">bankasampahbudiluhur@gmail.com</a></p>
+										<p><a href="mailto:services@deneb.com">bsblservice.com</a></p>
 									</div>
-									<div class="single_info">
-										<div class="icon">
-											<i class="fas fa-map-marker-alt"></i>
-										</div>
-										<div class="info">
-											<p>
-												<a>Jl. H. Gaim No.50, RT.10/RW.2, Petukangan Utara, Kec. Pesanggrahan, Kota
-													Jakarta Selatan, Daerah Khusus Ibukota Jakarta.</a></p>
-										</div>
+								</div>
+								<div class="single_info">
+									<div class="icon">
+										<i class="fas fa-map-marker-alt"></i>
+									</div>
+									<div class="info">
+										<p>
+											<a>Jl. H. Gaim No.50, RT.10/RW.2, Petukangan Utara, Kec. Pesanggrahan, Kota
+												Jakarta Selatan, Daerah Khusus Ibukota Jakarta.</a></p>
 									</div>
 								</div>
 							</div>
@@ -221,19 +171,20 @@
 					</div>
 				</div>
 			</div>
-			<div class="copyright_area">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-12">
-							<div class="copyright_text">
-								<p>Copyright &copy; 2020 All rights reserved.</p>
-							</div>
+		</div>
+		<div class="copyright_area">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="copyright_text">
+							<p>Copyright &copy; 2020 All rights reserved.</p>
 						</div>
 					</div>
 				</div>
 			</div>
-		</footer>
+		</div>
+	</footer>
 
-		<!-- footer section ends -->
-	</body>
+	<!-- footer section ends -->
+</body>
 <?= $this->endSection(); ?>

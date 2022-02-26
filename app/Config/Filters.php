e79f8b89703170a90e3da09f9419228a1cbade46
korospace <bagaselkoro@gmail.com>
2022-02-26 09:58:34 +0700

    fix cors file

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -6,6 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
+use App\Filters\Cors;
 
 class Filters extends BaseConfig
 {
@@ -19,7 +20,7 @@ class Filters extends BaseConfig
         'csrf'     => CSRF::class,
         'toolbar'  => DebugToolbar::class,
         'honeypot' => Honeypot::class,
-        'cors'     => App\Filters\Cors::class,
+        'cors'     => Cors::class,
     ];
 
     /**

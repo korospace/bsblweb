7f5254f8cdacd1850322a6738e23ad499486dc50
Lasteinsa <mikueinsa@gmail.com>
2022-06-02 20:33:29 +0700

    create penghargaan descriptions

--- app/Views/Components/artikelFooter.php
+++ app/Views/Components/artikelFooter.php
@@ -104,61 +104,6 @@
                             <li><a href="#"><i class="fab fa-instagram"></i></a></li>
                         </div>
                     </div>
-
-                    <div class="col-12 col-md-5 mt-5 mt-md-0">
-                        <div class="widget widget_contact">
-                            <h4 class="text-white" style="font-weight: bold;">Contact Us</h4>
-                            <div class="mt-4">
-                                <div class="d-flex">
-                                    <div class="icon mr-3">
-                                        <i class="fas fa-phone-alt" style="color:#C1D966;"></i>
-                                    </div>
-                                    <div class="info d-flex flex-column" style="line-height: 1.5;">
-                                        <a class="text-white" target="_blank" href="https://wa.me/6281385624543?text=Assalamualaikum%20Umi%20Utik,%20saya%20ingin%20bertanya%20mengenai%20banksampah%20budiluhur">
-                                            +62 813-8562-4543
-                                        </a>
-                                        <a class="text-white" target="_blank" href="https://wa.me/6287871911407?text=Assalamualaikum%20Umi%20Utik,%20saya%20ingin%20bertanya%20mengenai%20banksampah%20budiluhur">
-                                            +62 878-7191-1407
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="d-flex mt-3">
-                                    <div class="icon mr-3">
-                                        <i class="fas fa-envelope" style="color:#C1D966;"></i>
-                                    </div>
-                                    <div class="info d-flex flex-column" style="line-height: 1.5;">
-                                        <a class="text-white" href="bankasampahbudiluhur@gmail.com">
-                                            bankasampahbudiluhur@gmail.com
-                                        </a>
-                                        <a class="text-white" href="mailto:bsblservice@gmail.com">
-                                            bsblservice@gmail.com
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="d-flex mt-3">
-                                    <div class="icon mr-3">
-                                        <i class="fas fa-map-marker-alt" style="color:#C1D966;"></i>
-                                    </div>
-                                    <div class="info text-white" style="line-height: 1.5;">
-                                        Jl. H. Gaim No.50, RT.10/RW.2, Petukangan Utara, Kec. Pesanggrahan, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-        
-                    <div class="col-12 col-md-5 mt-5 mt-md-0">
-                        <div class="widget widegt_about">
-                            <h4 class="text-white" style="font-weight: bold;">Social media</h4>
-                            <!-- <p class="mt-4 text-white">Quisque orci nisl, viverra et sem ac, tincidunt egestas massa. Morbi est arcu, hendrerit ac vehicula condimentum, euismod nec tortor praesent consequat urna.</p> -->
-                            <div class="social">
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
 
             </div>
--- public/assets/js/listPenghargaan.js
+++ public/assets/js/listPenghargaan.js
@@ -11,8 +11,8 @@ axios.get(`${APIURL}/penghargaan/getpenghargaan`)
                     <div style="max-height:180px;overflow:hidden;">
                         <img class="img-thumbnail" src="${e.icon}" style="width:100%;"/>
                     </div>
-                    <p class="text-center text-uppercase" style="margin-top:10px;display: -webkit-box;-webkit-line-clamp: 2;-webkit-box-orient: vertical;overflow: hidden;text-overflow: ellipsis;">
-                        ${e.description}
+                    <p class="text-center text-uppercase px-3" style="margin-top:10px;display: -webkit-box;-webkit-line-clamp: 2;-webkit-box-orient: vertical;overflow: hidden;text-overflow: ellipsis;">
+                        ${e.name}
                     </p>
                 </div>
             </div>`;
@@ -35,9 +35,12 @@ axios.get(`${APIURL}/penghargaan/getpenghargaan`)
 /**
  * Card On Click
  */
-let zoomBg = document.querySelector("#zoom_bg");
-let zoomImg = document.querySelector("#zoom_bg #zoom_img")
+let zoomBg    = document.querySelector("#zoom_bg");
+let zoomImg   = document.querySelector("#zoom_bg #zoom_img")
+let zoomDes   = document.querySelector("#zoom_bg #zoom_des")
+let readMore  = document.querySelector("#zoom_bg #read_more")
 let zoomClose = document.querySelector("#zoom_bg #zoom_close")
+
 function cardOnClick(el) {
     let icon = el.dataset.icon;
     let desc = el.dataset.desc;
@@ -45,8 +48,14 @@ function cardOnClick(el) {
     document.body.style.overflow="hidden";
     zoomBg.classList.remove("hide");
     zoomImg.src = icon;
+    // zoomDes.innerHTML = desc;
     console.log();
 }
+readMore.addEventListener("click", () => {
+    $("#zoom_bg #zoom_des").css({
+        "-webkit-line-clamp": "2000"
+    });
+})
 zoomClose.addEventListener("click", () => {
     document.body.style.overflow="auto";
     zoomBg.classList.add("hide");
